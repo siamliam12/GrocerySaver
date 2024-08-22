5342ca19f4b6b73464e91048bc0f93ea8186c4f6
siamliam12 <siamahmed222945@gmail.com>
2024-08-22 11:27:17 +0600

    test

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ set -o errexit
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r ./backend/requirements.txt
 
-# Convert static asset files
-python ./backend/manage.py collectstatic --no-input
+# # Convert static asset files
+# python ./backend/manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python ./backend/manage.py migrate
