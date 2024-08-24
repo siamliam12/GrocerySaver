1b9c10aed43c2ffcd31d5774daf1e9147cad9818
siamliam12 <siamahmed222945@gmail.com>
2024-08-24 23:58:19 +0600

    render upload test

--- backend/build.sh
+++ backend/build.sh
@@ -5,6 +5,7 @@ set -o errexit
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r ./backend/requirements.txt
 
+python.exe -m pip install --upgrade pip
 # Convert static asset files
 python manage.py collectstatic --no-input
 
