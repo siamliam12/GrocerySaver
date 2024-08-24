c0644cb042aeb1eb4af198529182213b05793385
siamliam12 <siamahmed222945@gmail.com>
2024-08-25 00:06:08 +0600

    render upload test

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
