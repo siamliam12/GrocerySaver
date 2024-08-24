955f5fed491a7b1a66c970922a2aebbced3c9248
siamliam12 <siamahmed222945@gmail.com>
2024-08-25 00:04:26 +0600

    render upload test

--- backend/build.sh
+++ backend/build.sh
@@ -7,8 +7,8 @@ pip install -r ./backend/requirements.txt
 
 python -m pip install --upgrade pip
 # Convert static asset files
-python manage.py collectstatic --no-input
+python ./backend/manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
-python manage.py makemigrations
-python manage.py migrate
+python ./baackend/manage.py makemigrations
+python ./backend/manage.py migrate
